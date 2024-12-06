da1ae48313bcbc2453fc1f325f24aaf87422bd0b
dpiatkowski <damian.dem@gmail.com>
2024-12-06 20:23:08 +0100

    formatting?

--- src/colors_test.ts
+++ src/colors_test.ts
@@ -2,9 +2,9 @@ import { assertEquals } from "@std/assert";
 import { pixelsToRgb } from "./colors.ts";
 
 Deno.test("Mapping array to RGB object", () => {
-    const [red, green, blue] = [255, 64, 10];
+  const [red, green, blue] = [255, 64, 10];
 
-    const rgb = pixelsToRgb([red, green, blue]);
+  const rgb = pixelsToRgb([red, green, blue]);
 
-    assertEquals(rgb, { red: red, green: green, blue: blue });
+  assertEquals(rgb, { red: red, green: green, blue: blue });
 });
